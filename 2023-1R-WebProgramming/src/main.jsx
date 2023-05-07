858f9181a3cbe1a2a6c68a5eb33c952a6a9b8459
KimTaeUk <xodnr0130@naver.com>
2023-05-07 19:43:56 +0900

    Comment: main.jsx에서 StrictMode, <App /> 주석 추가

--- 2023-1R-WebProgramming/src/main.jsx
+++ 2023-1R-WebProgramming/src/main.jsx
@@ -7,4 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+    // React.StrictMode는 React에서 없엘 기능들을 사용하면 알려주는 component임
+    // <App />에 보여주고 싶은 component를 입력하면 됨
 )
