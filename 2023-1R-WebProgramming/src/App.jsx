b313c5b45106140a986bff84e280cd35b31a878c
KimTaeUk <xodnr0130@naver.com>
2023-05-07 19:40:07 +0900

    Feat: 미세먼지 API 화면에 rendering

--- 2023-1R-WebProgramming/src/App.jsx
+++ 2023-1R-WebProgramming/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [row, setRow] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    if (row.length === 0) {
+        fetch("http://openapi.seoul.go.kr:8088/5a46486149786f6439305372514a6f/json/RealtimeCityAir/1/25")
+            .then(function (res2) {
+                res2.json().then(function (res3) {
+                    setRow(res3.RealtimeCityAir.row);
+                });
+            });
+    }
+
+    return (
+        <>
+
+            <table>
+                <thead>
+                <th>이름</th>
+                <th>PM10</th>
+                <th>03</th>
+                <th>상태</th>
+                </thead>
+                <tbody>
+                {
+                    row.map(function (obj) {
+                        return <tr>
+                            <td>{obj.MSRSTE_NM}</td>
+                            <td>{obj.PM10}</td>
+                            <td>{obj.O3}</td>
+                            <td>{obj.IDEX_NM}</td>
+                        </tr>
+                    })
+                }
+                </tbody>
+            </table>
+        </>
+    );
 }
 
 export default App
