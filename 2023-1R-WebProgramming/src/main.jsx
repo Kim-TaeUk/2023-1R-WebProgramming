1d482faf35daee5faacf2e13273e8242027bfeca
KimTaeUk <xodnr0130@naver.com>
2023-05-15 02:39:06 +0900

    Feat: 이상형 월드컵(animation 구현X)

--- 2023-1R-WebProgramming/src/main.jsx
+++ 2023-1R-WebProgramming/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Worldcup from "./Worldcup.jsx";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Worldcup/>
+    </React.StrictMode>,
     // React.StrictMode는 React에서 없엘 기능들을 사용하면 알려주는 component임
     // <App />에 보여주고 싶은 component를 입력하면 됨
 )
